15ea2cb10b0dbbb46e6679a0292fc2627b2b0670
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2021-11-26 12:32:56 +0100

    Linked main page to Title component

--- components/title/index.js
+++ components/title/index.js
@@ -1,9 +1,12 @@
 import styles from "./title.module.css";
+import Link from "next/link";
 
 export default function Title(){
     return(
         <h1 className={styles.title}>
+        <Link href='/'>
         <a>Outella</a>
+        </Link>
       </h1>
     )
 }
