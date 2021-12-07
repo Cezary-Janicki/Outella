e36d1ff10fcfe034bcb7e5b00bc04a4769a01134
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2021-12-07 18:34:49 +0100

    Added image fade to the gallery Added text on fade WIP the fade moves images

--- components/image_gallery/index.js
+++ components/image_gallery/index.js
@@ -1,7 +1,6 @@
 import styles from "./image_gallery.module.css"
 import Link from "next/link"
 import Image from "next/image"
-import Gallery_Sidebar from "../gallery_sidebar"
 
 import leon7 from "../../public/vertical/leon7.jpg"
 export default function Image_Gallery(){
@@ -9,66 +8,86 @@ export default function Image_Gallery(){
     <div className={styles.grid}>
 
         <div className={styles.imageWrapper}>
-            <Image
+            <div className={styles.image}><Image
             alt="Dress"
             src={leon7}
             width={380}
             height={510}
-            />
+            /></div>
+            <div className={styles.middle}>
+             <div className={styles.text}>John Doe</div>
+            </div>
+            
         </div>
 
-        <div className={styles.imageWrapper}>
-            <Image
-            alt="Dress"
-            src={leon7}
-            width={380}
-            height={510}
-            />
-        </div>
 
         <div className={styles.imageWrapper}>
-            <Image
+            <div className={styles.image}><Image
             alt="Dress"
             src={leon7}
             width={380}
             height={510}
-            />
+            /></div>
+            <div className={styles.middle}>
+             <div className={styles.text}>John Doe</div>
+            </div>
+            
         </div>
 
-        <div className={styles.imageWrapper}>
-            <Image
-            alt="Dress"
-            src={leon7}
-            width={380}
-            height={510}
-            />
-        </div>
 
         <div className={styles.imageWrapper}>
-            <Image
+            <div className={styles.image}><Image
             alt="Dress"
             src={leon7}
             width={380}
             height={510}
-            />
+            /></div>
+            <div className={styles.middle}>
+             <div className={styles.text}>John Doe</div>
+            </div>
+            
         </div>
 
-        <div className={styles.imageWrapper}>
-            <Image
-            alt="Dress"
-            src={leon7}
-            width={380}
-            height={510}
-            />
-        </div>
 
         <div className={styles.imageWrapper}>
-            <Image
+            <div className={styles.image}><Image
             alt="Dress"
             src={leon7}
             width={380}
             height={510}
-            />
+            /></div>
+            <div className={styles.middle}>
+             <div className={styles.text}>John Doe</div>
+            </div>
+            
+        </div>
+
+
+        <div className={styles.imageWrapper}>
+            <div className={styles.image}><Image
+            alt="Dress"
+            src={leon7}
+            width={380}
+            height={510}
+            /></div>
+            <div className={styles.middle}>
+             <div className={styles.text}>John Doe</div>
+            </div>
+            
+        </div>
+
+    
+        <div className={styles.imageWrapper}>
+            <div className={styles.image}><Image
+            alt="Dress"
+            src={leon7}
+            width={380}
+            height={510}
+            /></div>
+            <div className={styles.middle}>
+             <div className={styles.text}>John Doe</div>
+            </div>
+            
         </div>
 
     </div>
