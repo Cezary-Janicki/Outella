067d488c8c6b9b8214844c11439e4648465bc02c
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2021-12-17 16:23:35 +0100

    Added basic face to main photos Added multiple images

--- components/main_photos/index.js
+++ components/main_photos/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import styles from "./main_photos.module.css" ;
 import leon2 from "../../public/horizontal/leon2.jpg" ;
+import leon3 from "../../public/horizontal/leon3.jpg";
 
 export default function Main_Photos(){
     return(
@@ -9,12 +10,24 @@ export default function Main_Photos(){
           {/*==================== M A I N _ P H O T O S ====================>*/}
           <div className={styles.gridBox}>
             <div className={styles.imageWrapper}>
+              <div className={styles.image1}>
               <Image
                 alt="Next.js logo"
                 src={leon2}
                 layout="fill"
                 objectFit="cover"
               />
+              </div>
+
+              <div className={styles.image2}>
+              <Image
+                alt="Next.js logo"
+                src={leon3}
+                layout="fill"
+                objectFit="cover"
+              />
+              </div>
+           
             </div>
           </div>
         </div>
