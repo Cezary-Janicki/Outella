55ac44f8fbfb94f9c3f5e47494f2fe2eaec2b496
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2022-01-12 09:38:45 +0100

    changed the slider gallery

--- components/slider_gallery/index.js
+++ components/slider_gallery/index.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 import Image from "next/image"
 
 import React from 'react';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext,DotGroup } from 'pure-react-carousel';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext,Dot,DotGroup } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
 
@@ -16,6 +16,7 @@ import leon5 from "../../public/horizontal/leon5.jpg"
 
 export default function Slider_Gallery({image1}){
     return(
+        <div className={styles.main}>
         <CarouselProvider
         naturalSlideHeight={125}
         naturalSlideWidth={100}
@@ -26,27 +27,41 @@ export default function Slider_Gallery({image1}){
         
         >
             <Slider>
-              <Slide index={0}><Link href="../../../"><Image
+              <Slide index={0} className={styles.slider}><Link href="../../../"><Image
                               alt="Next.js logo"
                               src={image1}
                               layout="fill"
                               objectFit="cover" /></Link>
                               </Slide>
-              <Slide index={1}><Image 
+              <Slide index={1} className={styles.slider}><Image 
                               alt="Leon the cat"
                               src={leon2}
                               layout="fill"
                               objectFit="cover" /></Slide>
-              <Slide index={2}><Image 
+              <Slide index={2} className={styles.slider}><Image 
                               alt="Leon the cat"
                               src={leon3}
                               layout="fill"
                               objectFit="cover" /></Slide>
             </Slider>
-            
-            <ButtonBack>Back</ButtonBack>
-            <ButtonNext>Next</ButtonNext>
-            <DotGroup />
+            <div className={styles.navigation}>
+            <div className={styles.navigationButtons}>
+                <ButtonBack className={styles.button}>⮜</ButtonBack>
+                <ButtonNext className={styles.button}>⮞</ButtonNext> </div>
+                <div className={styles.dotGroup}>
+                    <Dot slide={0} className={styles.dot}/>
+                    <Dot slide={1} className={styles.dot}/>
+                    <Dot slide={2} className={styles.dot}/>
+                </div>
+            </div>
         </CarouselProvider>
+        </div>
     )
+    
 }
+// TO DO
+// -arrows need to light up when clicked
+// -time bar to next transition needs to be added
+// -make the gallery responsive
+// -dots should be smaller on mobile(maybe diffrent solution ,divided horizontal unclickable bar?)
+// -images on mobile are unswipeable
