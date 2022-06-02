0b662aa80757894be62fac6c21510165b1c7ae95
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2022-06-02 16:47:13 +0200

    Update [id].js

--- pages/posts/products/[id].js
+++ pages/posts/products/[id].js
@@ -15,7 +15,7 @@ import Product_Page_Gallery from "../../../components/product_page_gallery";
 import EmblaCarousel from "../../../components/embla_carousel/image_carousel";
 
 import { getAllProductsIds,getProductsData  } from "../../../lib/products";
-import { useAppContext } from "../../../context/state";
+import { useAppContext, updateAppContext } from "../../../context/state";    //added update app context
 
 
 
@@ -35,6 +35,11 @@ export async function getStaticProps({params}){
     }
   } 
 }
+
+// export function updateAppContext({allProductsData}){
+//  how to properly update the function ?
+//   
+// }
 export default function bla({allProductsData}) {
 
   //radio buttons size selector code
