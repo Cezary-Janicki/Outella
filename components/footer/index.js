32213f22b6e2e3f769708f3cce45f55f8c474edb
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2022-06-14 15:39:57 +0200

    Added footer wrapper

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,9 +1,7 @@
 import styles from "./footer.module.css" 
 import Link from "next/link"
 import { getSortedPostsData } from "../../lib/posts";
-
-
-
+import Footer_Wrapper from "./footer_wrapper"
 export async function getStaticProps() {
   // const allPostsData = getSortedProductsData();
   const allPostsData =getSortedPostsData();
@@ -34,128 +32,27 @@ export default function footer({  }) {
                                  {/* If i pass a string or list its displayed correctly */}
 <div className={styles.background}>
 <footer className={styles.footer}>
-      <div className={styles.columns}>
-      <ul className={styles.list}>
-      <section>
-        <div className={styles.rows}>
-            <h2>Informacje</h2>
-        </div>
+
+<Footer_Wrapper>
+  <h2>Informacje</h2>
         {foo.map((d, index)=>(
           <div key={index} className={styles.mapa}>
           <Link href={`/posts/${d.id}`} className={styles.rows}>
           <p>{d.title}</p>
           </Link>
           </div>
-        ))}
-      </section>
-        
-      </ul>
-      </div>  
-      <div className={styles.columns} />
-
-      <div className={styles.columns}>
-      <div className={styles.rows}>
-                  <h2>Dane firmy</h2>
-                  </div>
-                  <div className={styles.rows}>
-                  <p>Cezary Janicki</p>
-                  <p>Outella</p>
-                  <p>Email: Cezary.D.Janicki@gmail.com</p>
-                  <p>NIP:7792517402</p>
-                  <p>REGON:386684150</p>
-                  </div>
-      </div>
-    
-{/*     
-        <div className={styles.columns}>
-          <div className={styles.rows}>
-            <h2>Informacje</h2>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/footer/header1/row1">
           
-            <p><a>O firmie</a></p>
-            </Link>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/footer/header1/row2">
-            <p><a>Regulamin</a></p>
-            </Link>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/footer/header1/row3">
-            <p><a>Polityka prywatności</a></p>
-            </Link>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/footer/header1/row4">
-            <p><a>Dostawa</a></p>
-            </Link>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/footer/header1/row5">
-            <p><a>Płatności</a></p>
-            </Link>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/footer/header1/row6">
-            <p><a>Kontakt</a></p>
-            </Link>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/footer/header1/row7">
-            <p><a>Polityka zwrotów</a></p>
-            </Link>
-          </div>
-        </div>
-        <div className={styles.columns}>
-          <div className={styles.rows}>
-            <h2>Zakupy</h2>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/products/Typ1/lorem">
-            <p><a>Typ sukienki 1</a></p>
-            </Link>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/products/Typ2/ipsum">
-            <p><a>Typ sukienki 2</a></p>
-            </Link>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/products/Typ3/dolor">
-            <p><a>Typ sukienki 3</a></p>
-            </Link>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/products/Typ4/sit">
-            <p><a>Typ sukienki 4</a></p>
-            </Link>
-          </div>
-          <div className={styles.rows}>
-            <Link href="/products/Typ5/amet">
-            <p><a>Typ sukienki 5</a></p>
-            </Link>
-          </div>
-        </div>
-        <div className={styles.columns}>
-          <div className={styles.rows}>
-            <h2>Dane Firmy</h2>
-          </div>
-          <div className={styles.rows}>
-            <p>Cezary Janicki</p>
-            <p>Outella</p>
-            <p>Email: Cezary.D.Janicki@gmail.com</p>
-            <p>NIP:7792517402</p>
-            <p>REGON:386684150</p>
-          </div>
-          <div className={styles.rows}>
-          </div>
-          <div className={styles.rows}>
-          </div>
-        </div> */}
+        ))}  
+</Footer_Wrapper>
 
-        
+<Footer_Wrapper>
+      <h2>Dane firmy</h2>                              
+      <p>Cezary Janicki</p>
+      <p>Outella</p>
+      <p>Email: Cezary.D.Janicki@gmail.com</p>
+      <p>NIP:7792517402</p>
+      <p>REGON:386684150</p>
+</Footer_Wrapper>
       </footer>
       </div>
       </div> 
