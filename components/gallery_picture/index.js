331e6106db2a9e7cfce15b3961dcf990c79bea72
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2022-11-23 10:01:48 +0100

    gallery wrapper is working properly with emotion

--- components/gallery_picture/index.js
+++ components/gallery_picture/index.js
@@ -1,14 +1,11 @@
 /** @jsxImportSource @emotion/react */
 
 // React components
-// import { css } from "@emotion/react";
 import React from "react";
-import styles from "./gallery_picture.module.css";
 import Link from "next/link";
 import Image from "next/image";
-// import Image_Wrapper from "../wrappers/image_wrapper";
 // import { GetSortedProductsData } from "../../lib/products";
-// import Gallery_Wrapper from "../wrappers/gallery_wrapper";
+import Gallery_Wrapper from "../wrappers/gallery_wrapper";
 
 const Gallery_Picture = (item) => {
   // const Gallery_Picture = ()=>{
@@ -16,32 +13,18 @@ const Gallery_Picture = (item) => {
   return (
     <>
       {item.item.map((d, index) => {
-        // let title = d.title;
+        let title = d.title;
         return (
           <Link key={index} href={`posts/products/${d.pictureName}${d.id}`}>
             <a>
-              {/* <Gallery_Wrapper title={title}>
+              <Gallery_Wrapper title={title}>
                 <Image
                   alt="Dress"
                   src={`/products/${d.pictureName}${d.id}/1/${d.pictureName}.jpeg`}
                   width={380}
                   height={510}
                 />
-              </Gallery_Wrapper> */}
-              <div className={styles.imageWrapper}>
-                <div className={styles.image}>
-                  <Image
-                    alt="Dress"
-                    src={`/products/${d.pictureName}${d.id}/1/${d.pictureName}.jpeg`}
-                    width={380}
-                    height={510}
-                  />
-                </div>
-
-                <div className={styles.middle}>
-                  <div className={styles.text}>{d.title}</div>
-                </div>
-              </div>
+              </Gallery_Wrapper>
             </a>
           </Link>
         );
--- components/wrappers/gallery_wrapper/index.js
+++ components/wrappers/gallery_wrapper/index.js
@@ -4,40 +4,27 @@
 // import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function Gallery_Wrapper(children, title) {
-  const ImageWrapper = styled.div`
-    width: 100%;
-    height: 100%;
-    position: relative;
-    cursor: pointer;
-  `;
+function Gallery_Wrapper({ children, title }) {
   const Image = styled.div`
-    opacity: 1;
-    display: block;
-    width: 100%;
-    height: auto;
-    backface-visibility: hidden;
-    transition: 0.5s ease;
-    // this should activate on hovering image wrapper
-    &:hover ${ImageWrapper} {
-      opacity: 0.6;
-      filter: blur(7px) brightness(80%) sepia(30%) hue-rotate(-70deg)
-        saturate(150%) contrast(1);
+     {
+      opacity: 1;
+      display: block;
+      width: 100%;
+      height: auto;
+      backface-visibility: hidden;
+      transition: 0.5s ease;
     }
   `;
   const Middle = styled.div`
+    // :hover ${ImageWrapper} {
+    //   opacity: 1;
+    // }
     transition: 0.5s ease;
     opacity: 0;
     position: relative;
-    bottom: 255px;
-    left: 190px;
-    transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    // this should activate on hovering image wrapper
-    &:hover ${ImageWrapper} {
-      opacity: 1;
-    }
+    transform: translate(0%, 1350%);
   `;
   const Text = styled.div`
     color: rgb(0, 0, 0);
@@ -46,17 +33,39 @@ export default function Gallery_Wrapper(children, title) {
     width: 100%;
     overflow: hidden;
   `;
+
+  const ImageWrapper = styled.div`
+    :hover ${Image} {
+      opacity: 0.6;
+      filter: blur(7px) brightness(80%) sepia(30%) hue-rotate(-70deg)
+        saturate(150%) contrast(1);
+    }
+    :hover ${Middle} {
+      opacity: 1;
+    }
+     {
+      width: 100%;
+      height: 100%;
+      position: relative;
+      cursor: pointer;
+    }
+  `;
+
   return (
     <div>
       {/* image_wrapper */}
       <ImageWrapper>
-        <Image alt="dress">{children}</Image>
-        {/* middle (text wrapper) */}
-
         <Middle>
           <Text> {title}</Text>
         </Middle>
+        <Image alt="dress">{children}</Image>
+        {/* middle (text wrapper) */}
       </ImageWrapper>
     </div>
   );
 }
+Gallery_Wrapper.propTypes = {
+  children: PropTypes.node,
+  title: PropTypes.string,
+};
+export default Gallery_Wrapper;
