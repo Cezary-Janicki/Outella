cd1ab93b57af97ef961c3f86fb15830215f3abe7
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2023-01-16 17:00:12 +0100

    added has mounted to body wrapper no main

--- components/wrappers/body_wrapper_no_main/index.js
+++ components/wrappers/body_wrapper_no_main/index.js
@@ -3,23 +3,31 @@
 //React components
 import React from "react";
 import PropTypes from "prop-types";
+import { useState } from "react";
+
 // import styles from "../../../styles/Home.module.css";
 import Head from "../../head";
 import Title from "../../title";
 import NavBar from "../../navbar";
 import Footer from "../../footer";
 import Scroll_Up_Button from "../../scroll_up_button";
-
+import ClientOnly from "../../clientOnly";
 function Body_Wrapper_No_Main({ children }) {
+  const [hasMounted, setHasMounted] = useState(false);
+
   return (
-    <div>
-      <Head />
-      <Title />
-      <NavBar />
-      <Scroll_Up_Button />
-      {children}
-      <Footer />
-    </div>
+    <ClientOnly>
+      {hasMounted === true ? <>{setHasMounted(false)}</> : null}
+      <div>
+        <Head />
+        <Title />
+        <NavBar />
+        <Scroll_Up_Button />
+
+        {children}
+        <Footer />
+      </div>
+    </ClientOnly>
   );
 }
 Body_Wrapper_No_Main.propTypes = {
