b19e376604127f55eda4026c8dadfcbb91a2b642
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2023-04-14 16:40:31 +0200

    Added headers for clarity

--- pages/footer/polityka.js
+++ pages/footer/polityka.js
@@ -3,7 +3,11 @@ import Body_Wrapper from "../../components/wrappers/body_wrapper";
 import React from "react";
 
 export default function Polityka() {
-  return <Body_Wrapper></Body_Wrapper>;
+  return (
+    <Body_Wrapper>
+      <h1>Polityka</h1>
+    </Body_Wrapper>
+  );
 }
 
 //this page generates the footer
--- pages/footer/regulamin.js
+++ pages/footer/regulamin.js
@@ -3,5 +3,9 @@ import Body_Wrapper from "../../components/wrappers/body_wrapper";
 import React from "react";
 
 export default function Regulamin() {
-  return <Body_Wrapper></Body_Wrapper>;
+  return (
+    <Body_Wrapper>
+      <h1>Regulamin</h1>
+    </Body_Wrapper>
+  );
 }
